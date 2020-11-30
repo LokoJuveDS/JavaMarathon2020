a6d82d3678a79e37eb4e7e00ceabc1acc0ce5094
Yegor Priimak <egorpryimak@gmail.com>
2020-11-30 15:12:36 +0300

    complete day1 tasks

--- src/main/java/day1/Task5.java
+++ src/main/java/day1/Task5.java
@@ -2,6 +2,12 @@ package day1;
 
 public class Task5 {
     public static void main(String[] args) {
+        int year = 1980;
+        for (int i = year; i <= 2020; i+=4) {
+            System.out.println("Олимпиада " + i + " года");
+        }
 
+        //при такой реализации year НЕ меняется к концу программы
+        //System.out.println(year);
     }
 }
